f415095caa875520dd8cf8320714af4f8c253973
Tomir Domanski <tomirdomanski@outlook.com>
2026-09-07 17:43:51 +0200

    Hello world

---
+++ Part1/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿// Using system - previous versions of C# required that at the beginning
+Console.WriteLine("Hello, World! 1");
+Console.WriteLine("Hello, World! 2");
+Console.WriteLine("Hello, World! 3");
+Console.WriteLine("Hello, "+"World! 4");
+string name;
+name = "User";
+Console.WriteLine("Hello " + name + "!");
+Console.WriteLine("Provide your name");
+name = Console.ReadLine();
+Console.WriteLine("Hello " + name + "!");
