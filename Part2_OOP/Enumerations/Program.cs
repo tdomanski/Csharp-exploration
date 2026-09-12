fd7957542d9a4bf995b47afda2511a6f4c34e0bc
Tomir Domanski <tomirdomanski@outlook.com>
2026-09-12 11:31:48 +0200

    Enumerations

---
+++ Part2_OOP/Enumerations/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Season currentSeason = Season.Summer;
+if (currentSeason == Season.Summer)
+{
+    Console.WriteLine("It's summer time!");
+}
+else
+{
+    Console.WriteLine("It's not summer.");
+}
+// Enumerations are always after other code
+enum Season { Winter, Spring, Summer, Fall }
