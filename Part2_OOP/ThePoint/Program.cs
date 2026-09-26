3c5061c860afcbfde69685ca9eef8df98ff46434
Tomir Domanski <tomirdomanski@outlook.com>
2026-09-26 06:32:42 +0200

    The Point - Boss Battle

---
+++ Part2_OOP/ThePoint/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Point firstPoint = new Point(2, 3);
+Point secondPoint = new Point(-4, 0);
+
+Console.WriteLine($"First point: ({firstPoint.x}, {firstPoint.y})");
+Console.WriteLine($"Second point: ({secondPoint.x}, {secondPoint.y})");
+
+public class Point
+{
+    public int x { get; set; }
+    public int y { get; set; }
+    public Point()
+    {
+        this.x = 0;
+        this.y = 0;
+    }
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+}
